5c39f11f8ee4305b41fd4328856de427e0c4bf9c
classic89 <retrorock8821@gmail.com>
2018-10-11 00:33:45 -0500

    Demo on config vars

--- hello/views.py
+++ hello/views.py
@@ -1,15 +1,15 @@
-import requests
-
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
 
+import requests
+import os
+
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    times = int(os.environ.get('TIMES',3))
+    return HttpResponse('Hello! ' * times)
 
 def db(request):
 
