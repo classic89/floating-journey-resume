6783aed99be916beeb3a3b89862f0faf63f34311
classic89 <retrorock8821@gmail.com>
2018-10-10 23:25:58 -0500

    Demo

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,5 @@
+import requests
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,9 +7,9 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
-
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 def db(request):
 
@@ -17,4 +19,3 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
-
