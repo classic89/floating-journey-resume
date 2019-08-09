f6caf8d7c8a9adac66d8055fa26df6912da551a1
Classic ML Wilson <classic@meganlynnwilson.org>
2019-08-09 06:31:32 -0400

    cleanup

--- myresume/models.py
+++ myresume/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # myresume/models.py
 
-class Todo(models.Model):
+class myresume(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
 
